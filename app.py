a927e8da1ea3c81a4919709392c9b32296eedf44
Camilla Heleno <camiheleno@gmail.com>
2018-11-11 15:23:24 -0200

    Documenting get modalities function.

--- app.py
+++ app.py
@@ -1,55 +1,63 @@
 import datetime
 from bson.json_util import dumps
+from flasgger import Swagger
+from flasgger.utils import swag_from, validate
 from flask import Flask
 from flask import request
 from flask import jsonify
-from pymongo import MongoClient, DESCENDING
+from pymongo import MongoClient
 import sys
 
 app = Flask(__name__)
+Swagger(app)
 
 @app.route("/")
 def hello():
     return "Hey I'm using Docker!"
 
 @app.route('/modalities', methods = ['GET'])
+@swag_from('docs/get_modalities.yml')
 def get_modalities():
-    status_code = 200
-    json_response = {}
-    if 'modalidade' not in request.args:
-        json_response = {'code': 2, 'status': 'error', 'message': 'No modality was informed.'}
-        status_code = 500
-    elif 'data_inicio' not in request.args: 
-        json_response = {'code': 3, 'status': 'error', 'message': 'No inital date was informed.'}
-        status_code = 500
-    elif 'data_fim' not in request.args: 
-        json_response = {'code': 4, 'status': 'error', 'message': 'No final date was informed.'}
-        status_code = 500
+    try:
+        status_code = 200
+        json_response = {}
+        if 'modalidade' not in request.args:
+            json_response = {'code': 2, 'status': 'error', 'message': 'No modality was informed.'}
+            status_code = 412
+        elif 'data_inicio' not in request.args: 
+            json_response = {'code': 3, 'status': 'error', 'message': 'No inital date was informed.'}
+            status_code = 412
+        elif 'data_fim' not in request.args: 
+            json_response = {'code': 4, 'status': 'error', 'message': 'No final date was informed.'}
+            status_code = 412
 
-    if status_code != 500:
-        client = MongoClient('142.44.178.41', 27017)
-        db = client.camilla
-        collection = db.estudantes
+        if status_code != 412:
+            client = MongoClient('142.44.178.41', 27017)
+            db = client.camilla
+            collection = db.estudantes
 
-        final_date = datetime.datetime.strptime(request.args['data_fim'], '%Y-%m-%d')
-        initial_date = datetime.datetime.strptime(request.args['data_inicio'], '%Y-%m-%d')
+            final_date = datetime.datetime.strptime(request.args['data_fim'], '%Y-%m-%d')
+            initial_date = datetime.datetime.strptime(request.args['data_inicio'], '%Y-%m-%d')
 
-        print(final_date, file=sys.stderr)
-        print(initial_date, file=sys.stderr)
-        modalities = collection.find(
-            {
-                "modalidade": request.args['modalidade'].upper(),
-                "data_inicio": {"$lte": final_date, "$gte": initial_date}
-            },
-            { "_id":0, "nome": 1, "idade_ate_31_12_2016": 1, "ra": 1, "campus": 1, "municipio": 1, "curso": 1, "modalidade": 1, "nivel_do_curso": 1, "data_inicio": 1 }
-        ).sort("data_inicio", DESCENDING)
+            print(final_date, file=sys.stderr)
+            print(initial_date, file=sys.stderr)
+            modalities = collection.find(
+                {
+                    "modalidade": request.args['modalidade'].upper(),
+                    "data_inicio": {"$lte": final_date, "$gte": initial_date}
+                },
+                { "_id":0, "nome": 1, "idade_ate_31_12_2016": 1, "ra": 1, "campus": 1, "municipio": 1, "curso": 1, "modalidade": 1, "nivel_do_curso": 1, "data_inicio": 1 }
+            ).sort("data_inicio", -1)
 
-        json_response = {'code': 1, 'status': 'success', 'data': list(modalities)}
+            json_response = {'code': 1, 'status': 'success', 'data': list(modalities)}
 
-    resp = jsonify(json_response)
-    resp.status_code = status_code
-
-    return resp
-
+        resp = jsonify(json_response)
+        resp.status_code = status_code
+    except:
+        resp = jsonify({'code': 99, 'status': 'error', 'message': sys.exc_info()[0]})
+        resp.status_code = 500
+    finally:
+        return resp
+        
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
